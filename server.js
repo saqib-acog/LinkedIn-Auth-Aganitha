c4e439ba5a3506bfc292c9eaa3b004bd9bce5846
Saqib Hussain <saqib@aganitha.ai>
2024-03-03 12:25:20 +0530

    cors policy not removed

--- server.js
+++ server.js
@@ -42,20 +42,22 @@ app.use(methodOverride("_method"));
 
 // Page to download the requested resource
 app.get("/", checkAuthenticated, (req, res) => {
-  const resourceUrl = req.session.resourceurl;
-  console.log(resourceUrl);
-  res.render("downloads.ejs", { resourceUrl });
+  const resourceUrl = req.query.resourceUrl;
+  const referer = req.query.referer;
+  res.render("downloads.ejs", { resourceUrl, referer });
 });
 
 // To redirect user to linkedin login page
 app.get("/auth/linkedin", checkNotAuthenticated, (req, res) => {
   const resourceUrl = req.query.resourceUrl;
+  const referer = req.query.referer;
   const clientId = process.env.LINKEDIN_CLIENT_ID;
   const redirectUri = encodeURIComponent(
-    `${process.env.LINKEDIN_CALLBACK}?resourceUrl=${resourceUrl}`
+    `${process.env.LINKEDIN_CALLBACK}?resourceUrl=${resourceUrl}&referer=${referer}`
   );
+  console.log(redirectUri);
   const state = Math.random().toString(36).substring(7);
-  // console.log(state);
+
   const scope = encodeURIComponent("openid profile email");
 
   const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
@@ -66,9 +68,11 @@ app.get("/auth/linkedin", checkNotAuthenticated, (req, res) => {
 app.get("/auth/linkedin/callback", checkNotAuthenticated, async (req, res) => {
   try {
     const resourceUrl = req.query.resourceUrl;
+    console.log(resourceUrl);
+    const referer = req.query.referer;
     const code = req.query.code;
     const redirectUri = encodeURIComponent(
-      `${process.env.LINKEDIN_CALLBACK}?resourceUrl=${resourceUrl}`
+      `${process.env.LINKEDIN_CALLBACK}?resourceUrl=${resourceUrl}&referer=${referer}`
     );
     const accessToken = await getAccessToken(code, redirectUri);
     const userInfo = await getUserInfo(accessToken);
@@ -78,9 +82,9 @@ app.get("/auth/linkedin/callback", checkNotAuthenticated, async (req, res) => {
     appendUserData(userInfo);
 
     req.session.user = userInfo;
-    req.session.resourceurl = resourceUrl;
-
-    res.redirect("/");
+    res.redirect(
+      `/?action=download&resourceUrl=${resourceUrl}&referer=${referer}`
+    );
   } catch (error) {
     res.status(500).json({
       message: `Internal Server Error, ${error}`,
@@ -89,11 +93,12 @@ app.get("/auth/linkedin/callback", checkNotAuthenticated, async (req, res) => {
 });
 
 function checkAuthenticated(req, res, next) {
+  const resourceUrl = req.query.resourceUrl;
+  const referer = req.header("Referer");
   if (req.session.user) {
     return next();
   }
-  const resourceUrl = req.query.resourceUrl;
-  res.redirect(`/auth/linkedin?resourceUrl=${resourceUrl}`);
+  res.redirect(`/auth/linkedin?resourceUrl=${resourceUrl}&referer=${referer}`);
 }
 
 function checkNotAuthenticated(req, res, next) {
